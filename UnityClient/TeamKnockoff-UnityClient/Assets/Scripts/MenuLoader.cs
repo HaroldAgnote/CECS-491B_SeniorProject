e1ec0ff63ecd9183c25bb2baca2b7575681fe620
Harold Agnote <haroldagnote38@gmail.com>
2019-02-06 20:28:28 -0800

    Refactored menus
    - Resized menus to correct aspect ration (1080p - 1920 x 1080)
    - Reorganized menu system
    - Implemented menu loader script
    - Disabled display resolution dialog box on start up

---
+++ UnityClient/TeamKnockoff-UnityClient/Assets/Scripts/MenuLoader.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuLoader : MonoBehaviour
+{
+    const int MAIN_MENU_INDEX = 0;
+    const int SINGLEPLAYER_MENU_INDEX = 1;
+    const int CAMPAIGN_MENU_INDEX = 2;
+    const int MULTIPLAYER_MENU_INDEX = 3;
+
+    public void GoToMenu(int menuIndex) {
+        SceneManager.LoadScene(menuIndex);
+    }
+
+    public void GoToMainMenu() {
+        GoToMenu(MAIN_MENU_INDEX);
+    }
+
+    public void GoToSingleplayerMenu() {
+        GoToMenu(SINGLEPLAYER_MENU_INDEX);
+    }
+
+    public void GoToCampaignMenu() {
+        GoToMenu(CAMPAIGN_MENU_INDEX);
+    }
+
+    public void GoToMultiplayerMenu() {
+        GoToMenu(MULTIPLAYER_MENU_INDEX);
+    }
+
+    public void QuitGame ()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+}
