33bf2112009009633426b5484c6fc279f33f45d7
Harold Agnote <haroldagnote38@gmail.com>
2019-02-10 22:52:54 -0800

    Moved Unit base class to Unit folder and added SampleUnit

---
+++ UnityClient/TeamKnockoff-UnityClient/Assets/Scripts/Units/SampleUnit.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Units {
+    public class SampleUnit : Unit {
+        public SampleUnit()
+        {
+            Name = "Sample Unit";
+            HealthPoints = 100;
+            MoveRange = 3;
+            AttackRange = 1;
+        }
+        public override List<Vector2Int> AttackLocations(Vector2Int gridPoint) {
+            throw new NotImplementedException();
+        }
+
+        public override List<Vector2Int> MoveLocations(Vector2Int gridPoint) {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UnityClient/TeamKnockoff-UnityClient/Assets/Scripts/Units/Unit.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Unit : MonoBehaviour
+{
+    public double HealthPoints { get; set; }
+
+    public int MoveRange { get; set; }
+
+    public int AttackRange { get; set; }
+
+    public string Name { get; set; }
+
+    public abstract List<Vector2Int> MoveLocations(Vector2Int gridPoint);
+    public abstract List<Vector2Int> AttackLocations(Vector2Int gridPoint);
+}
