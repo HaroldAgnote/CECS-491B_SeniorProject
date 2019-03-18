6ef63af70499324da8004e7fa01d4194d4d4d0a7
Harold Agnote <haroldagnote38@gmail.com>
2019-03-18 15:15:10 -0700

    Moving fonts and fixing bug in CPU Opponent

--- UnityClient/TeamKnockoff-UnityClient/Assets/Scripts/ComputerOpponent/ComputerOpponent.cs
+++ UnityClient/TeamKnockoff-UnityClient/Assets/Scripts/ComputerOpponent/ComputerOpponent.cs
@@ -6,7 +6,7 @@ using Assets.Scripts.Model.Units;
 
 namespace Assets.Scripts.ComputerOpponent
 {
-    public class ComputerOpponent 
+    public class ComputerOpponent : MonoBehaviour
     {
         //Use all possible units to attack
         //Move other units closer to enemy positions
